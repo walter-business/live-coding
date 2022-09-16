5fed62ee894bc8c30b3b57cfd36d629f7bb15771
Walter Angerer <walter.angerer@allane.com>
2022-09-16 14:00:35 +0200

    remove Mapping annotation

--- backend/src/main/java/com/allane/bp/live/coding/mapper/CustomerMapper.java
+++ backend/src/main/java/com/allane/bp/live/coding/mapper/CustomerMapper.java
@@ -3,7 +3,6 @@ package com.allane.bp.live.coding.mapper;
 import com.allane.bp.live.coding.api.model.CustomerDTO;
 import com.allane.bp.live.coding.entity.CustomerEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
@@ -12,15 +11,7 @@ import org.mapstruct.ReportingPolicy;
 )
 public interface CustomerMapper {
 
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "firstName", source = "firstName")
-    @Mapping(target = "lastName", source = "lastName")
-    @Mapping(target = "birthDate", source = "birthDate")
     CustomerDTO toDto(CustomerEntity source);
 
-    @Mapping(target = "id", source = "id")
-    @Mapping(target = "firstName", source = "firstName")
-    @Mapping(target = "lastName", source = "lastName")
-    @Mapping(target = "birthDate", source = "birthDate")
     CustomerEntity toEntity(CustomerDTO source);
 }
