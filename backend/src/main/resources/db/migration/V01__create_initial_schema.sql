f92afdc5ff4cf6bf25c95e1890808e17006e281d
Walter Angerer <walter.angerer@allane.com>
2022-11-08 17:25:20 +0100

    align length of columns with open api spec

--- backend/src/main/resources/db/migration/V01__create_initial_schema.sql
+++ backend/src/main/resources/db/migration/V01__create_initial_schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE customer
 (
     id                   bigint(20)   NOT NULL AUTO_INCREMENT,
-    first_name           varchar(255) NOT NULL ,
-    last_name            varchar(255) NOT NULL,
+    first_name           varchar(50) NOT NULL ,
+    last_name            varchar(50) NOT NULL,
     birth_date           date         NOT NULL,
     PRIMARY KEY (id)
 );
