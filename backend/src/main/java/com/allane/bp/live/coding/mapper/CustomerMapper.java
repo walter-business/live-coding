26d433a1ee890ccba1c2850a30cd68ad8ecc7ae2
Walter Angerer <walter.angerer@allane.com>
2023-02-15 17:09:50 +0100

    use the constant for component model

--- backend/src/main/java/com/allane/bp/live/coding/mapper/CustomerMapper.java
+++ backend/src/main/java/com/allane/bp/live/coding/mapper/CustomerMapper.java
@@ -3,10 +3,11 @@ package com.allane.bp.live.coding.mapper;
 import com.allane.bp.live.coding.api.model.CustomerDTO;
 import com.allane.bp.live.coding.entity.CustomerEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
-        componentModel = "spring",
+        componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.ERROR
 )
 public interface CustomerMapper {
